48af6f4a7c4ca8453c5234d000778c1ec65404c6
Lai Shu Xian <sxlai2@illinois.edu>
2021-01-06 10:37:11 -0600

    fibo dp loop version

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int fib(n){
     vector<int> memo;
     int i;
-    memo.push_back(1);
+    memo.push_back(0);
     memo.push_back(1);
     for(i=2;i<=n;i++){
         int a = memo[i-1];
@@ -19,3 +19,9 @@ int fib(n){
 //how to code with dp with recursion
 //does cpp support the style <if ... in ...>
 //do recursion with something like .size()?
+
+int main(){
+    int test = fib(10);
+    cout << test << endl;
+    return 0;
+}

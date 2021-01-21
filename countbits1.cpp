a6d6f78f230b48c4b75e986ffe8b03296c8fb54e
Lai Shu Xian <sxlai2@illinois.edu>
2021-01-21 02:07:28 -0600

    debugging

---
+++ countbits1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> countBits(int num) {
+        vector<int> dp(num+1);
+        dp[0] = 0;
+        for(int i=1; i<=num;i++){
+            //if i is a number of 2 power, num of bits = 1;
+            //if i is odd, the number of bits of the previous + 1;
+            //if i is even and not a power of two; 
+            if((i&(i-1))==0){
+                dp.push_back(1);
+            }
+            else if(i%2 == 1){
+                dp.push_back(dp[i-1]+1);
+            }
+            else {
+                int index = i/2;
+                dp.push_back(dp[index]);
+            }
+        }
+        return dp;
+    }
+};
